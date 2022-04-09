a2aba85c101116614997c9bc04b3cfe9f048afe8
Anders <anders@Anders.Laptop>
2022-04-09 15:58:57 -0400

    Cafes

---
+++ src/data/cafes.js
@@ -0,0 +1,50 @@
+import LocalLion from '../images/coffeeshops/LocalLion.png'
+import WiredScholar from '../images/coffeeshops/WiredScholar.png'
+import PSU from '../images/coffeeshops/PSU.png'
+import BlueDeer from '../images/coffeeshops/BlueDeer.png'
+import HigherGround from '../images/coffeeshops/HigherGround.png'
+
+
+
+const cafes = [
+    {
+    name: "The Local Lion",
+    location: "791 Blowing Rock Rd, Boone, NC. 28607 USA ",
+    url:"https://local-lion.com/",
+    phone: "(828) 386-1120",
+    description: "The Local Lion is a family owned and operated business.  Josiah Davis, a Watauga County native, grew up in Boone and is a graduate of Watauga High School. He and his wife, Meredith, are graduates of Appalachian State University. Seeing that Boone was missing a drive-thru coffee shop and doughnut shop, the idea of the Local Lion was born. Josiah and Meredith found doughnut equipment for sale located in Roan Mountain, TN. Little did they know this discovery included the doughnut baking knowledge of  award winning bakers, Tim and Crystal Dekker. Tim and Crystal of Smoky Mountain Bakery, generously trained Josiah and Meredith in the art of making doughnuts from scratch the old fashioned way.",
+    image: LocalLion
+},
+{
+    name: "Wired Scholar",
+    location: "The Belk Library",
+    url:"https://dining.appstate.edu/dining-locations/crossroads-wired-scholar ",
+    phone: "",
+    description: "Located in the Belk Library and Information Commons, Wired Scholar is a study-time staple for students, faculty, and staff utilizing the study space and research resources in Appalachian State University's library.",
+    image: WiredScholar
+},
+{
+    name: "Cross Roads",
+    location: "Plemmons Student Union",
+    url:"https://dining.appstate.edu/dining-locations/crossroads-wired-scholar",
+    phone: "",
+    description: "Located in the Plemmons Student Union, Crossroads has been a part of study sessions, coffee breaks, meetings, and night-time entertainment since the 1990s.",
+    image: PSU
+},
+{
+    name: "Blue Deer",
+    location: "3551 U.S. 321, Blowing Rock, NC, 28605, United States",
+    url:"https://www.bluedeercookies.com/",
+    phone: "",
+    description: " ",
+    image: BlueDeer
+},
+{
+    name: "Higher Grounds Coffee Place",
+    location: "201 New Market Ctr # 10, Boone, NC 28607-3993 ",
+    url:"http://highergroundscoffeeshoppe.com/",
+    phone: "(828) 265-1114",
+    description: "We opened Higher Grounds Coffee Shoppe With the Purpose… Of bringing quality products to the town of Boone, North Carolina. After years in business, We are proud to say our focus has never been lost. We strive to make customer satisfaction our number one priority. We appreciate your business and support.",
+    image: HigherGround
+},
+]
