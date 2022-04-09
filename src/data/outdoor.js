d1cf97fc23d0d75516ce0d4f4de7d50678e17974
Anders <anders@Anders.Laptop>
2022-04-09 15:05:25 -0400

    Work Please

--- src/data/outdoor.js
+++ src/data/outdoor.js
@@ -1,9 +1,84 @@
+import HawkinsGame from '../images/activities/HawkinsGame.png'
+import BooneMall from '../images/activities/BooneMall.png'
+import MastGen from '../images/activities/MastGen.png'
+import Spa from '../images/activities/Spa.png'
+import Sporting from '../images/activities/Sporting.png'
+import Hachet from '../images/activities/Hachet.png'
+import Happy from '../images/activities/Happy.png'
 const Outdoor = [
     {
-        location: "",
-        phone: 0,
-        description: ""
+        name: "Hawkins Game Room",
+        location: "Plemmons Student Union",
+        url:"https://studentunion.appstate.edu/hawksbillgameroom",
+        phone: "(828) 262-6500",
+        description: "Hawksbill Gameroom has 6 billiard tables, 2 dart boards, 2 foosball tables, free board games and playing cards to use.  Pool, darts, foosball, and board games are free for students and nominal cost for faculty staff and visitors!",
+	    image: HawkinsGame
     },
+    {
+        name: "Boone Mall",
+        location: "1180 Blowing Rock Road, Boone, NC 28607",
+        url:"https://www.boonencmall.com/",
+        phone: "(828) 264-7286",
+        description: "",
+	    image: BooneMall
+    },
+    {
+        name: "Mast General Store",
+        location: "630 W King St, Boone, NC",
+        url:"https://www.mastgeneralstore.com/boone",
+        phone: "(866) 367-6278",
+        description: "The Mast Store in Downtown Boone resides in two buildings constructed in the early years of the 20th century. Many a memory was made and shared in the old Hunt’s Department Store, which is the main part of the Boone location. Its Y-shaped staircase has carried thousands of footsteps upstairs to find overalls and footwear. Today, it’s hard to find the old drain pipes for the sinks in the beauty shop once located inside, but what you will find is eager people ready to help you locate just what you are looking for and to share some helpful hints along the way.",
+	    image: MastGen
+    },
+    {
+        name: "Mysterium Escape Adventure",
+        location: "737 W. King Street Boone, NC 28607",
+        url:"https://www.mysteriumboone.com/",
+        phone: "(828) 865-0009",
+        description: "We are a local, homegrown escape room. Our themed escape rooms in Boone, NC, are made from scratch and our rooms are all built in-house. That means you will have a completely original experience!",
+	    image: Mys
+    },
+    {
+        name: "7th Heaven Day Spa",
+        location: "4457 Nc Highway 105 S, Boone, NC 28607-7319",
+        url:"https://7thheaven-boone.com/",
+        phone: "(828) 963-2355",
+        description: "The 7th Heaven Day Spa is a 1000 sq. ft. facility, on the upstairs level of the building and decorated in a peaceful celestial theme. 7th Heaven is conveniently located just outside the Boone town limits at 4457 Hwy. 105 South and is easily accessible to all of the High Country. Here you will find treatments to rival the most expensive spas, but without the cost.",
+	    image: Spa
+    },
+    {
+        name: "Rock Dimensions",
+        location: "139 S Depot St, Boone, NC 28607-4017",
+        url:"https://www.rockdimensions.com/",
+        phone: "(828) 265-3544",
+        description: " Rock Dimensions has been in business since August 1998, continuing to grow as we provide guided climbing trips and instruction to a variety of groups and individuals. We are PCIA certified climbing guides in Boone NC, dedicated to providing a safe and enjoyable climbing program for all of our clients.Now our downtown Boone location is a climbing gear shop in addition to our guide service. We are happy to supply climbers in Boone and the surrounding areas with the best equipment on the market.",
+	    image: Rock
+    },
+    {
+        name: "Chetola Sporting Reserve",
+        location: "185 Chetola Lake Drive PO Box 17 Blowing Rock, NC 28605 ",
+        url:"https://chetola.com/recreation/sporting-reserve/",
+        phone: "(800) 243-8652",
+        description: "n 67 acres of rural rolling terrain, among towering pines, red maples, and mighty oaks, and surrounded by the incredible Blue Ridge vistas in the heart of the gated Blue Ridge Mountain Club, lies Chetola Sporting Reserve. The private club, open exclusively to those with annual memberships or lodging guests at Chetola Resort, features an array of outdoor sportsman activities including gun, rifle, and pistol ranges, sporting clay course, 5-stand clay station, archery range, and Orvis® fly fishing excursions. As the first of only two Orvis® endorsed fly fishing lodges in North Carolina, you will find the best in wade, trophy, and float fishing, along with casting clinics, and even team-building with CSR experts at the helm. All activities are led by NRA-certified instructors specialized in a particular discipline, with customized Churchill-style instruction based on each guest’s proficiencies and needs. ",
+	    image: Sporting
+    },
+    {
+        name: "Elwood’s Hatchet Throwing",
+        location: "199-C Jefferson Road Boone, NC 28607",
+        url:"https://www.elwoodshatchethouse.com/",
+        phone: "(828) 406-3133",
+        description: "Elwood’s Hatchet House is proud to be Boone’s first & only indoor Axe Throwing Tavern featuring 8 axe throwing targets, 2 corn hole lanes, a giant connect four, pool table, air hockey, pinball, cold craft & domestic beer, and great food are waiting on you. Book a lane and grab an axe or just come hang out with us and drink a few cold beers.",
+	    image: Hachet
+    },
+    {
+        name: "The Happy Place",
+        location: "641 W King St, Boone, NC 28607",
+        url:"http://www.ahappyplacetobe.com/",
+        phone: "(828) 865-0222",
+        description: "We carefully choose products that represent and the bring out the eccentric and flavorful side of life. Multigenerational in our interest we can appeal to the lifestyle of any age!",
+	    image: Happy
+    },
+
 ]
 
 export default Outdoor
